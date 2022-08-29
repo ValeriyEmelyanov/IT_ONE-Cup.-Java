93ad5f20f9c7ca862a7a964a06f09d4837c47225
Valeriy Emelyanov <emelva@rambler.ru>
2022-08-29 13:48:47 +0300

    fix

--- minbenchmark/src/main/java/ru/vk/competition/minbenchmark/util/ConstraintsUtil.java
+++ minbenchmark/src/main/java/ru/vk/competition/minbenchmark/util/ConstraintsUtil.java
@@ -4,4 +4,7 @@ public class ConstraintsUtil {
     public static final String IDENTIFIER_PATTERN = "([^\\s,;]+)|(\"[^\"\u0000]+\")";
     public static final int TABLE_NAME_MAX_SIZE = 50;
     public static final int SQL_MAX_SIZE = 120;
+
+    private ConstraintsUtil() {
+    }
 }
